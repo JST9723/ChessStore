f7b9cdbb5fb2976e7f669cbd62ffba4aaa6f70ff
Siting Jin <sitingj@andrew.cmu.edu>
2017-05-06 22:30:52 -0400

    render action

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,5 +1,5 @@
 class SchoolsController < ApplicationController
-  before_action :set_school,[:show, :edit, :update]
+  before_action :set_school, only: [:show,:edit]
   before_action :check_login
 
   def index
@@ -13,17 +13,28 @@ class SchoolsController < ApplicationController
   end
 
   def show
+    authorize! :show, @school
   end
 
-  # def edit
-  # end
-  #
-  # def create
-  # end
+  def edit
+  end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to school_path(@school), notice: "Successfully created #{@school.name}."
+    else
+      render action: 'new'
+    end
+  end
 
   private
   def set_school
     @school = School.find(params[:id])
+  end
+
+  def school_params
+      params.require(:school).permit(:name, :street_1, :street_2, :city, :state, :zip, :min_grade, :max_grade, :active)
   end
 
 
