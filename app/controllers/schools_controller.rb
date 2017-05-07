04da0031abc7327ef6bcfecba274b8da74b32a77
Siting Jin <sitingj@andrew.cmu.edu>
2017-05-07 02:14:08 -0400

    school actions

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,5 +1,5 @@
 class SchoolsController < ApplicationController
-  before_action :set_school, only: [:show,:edit]
+  before_action :set_school, only: [:show,:edit, :update,:destroy]
   before_action :check_login
 
   def index
@@ -26,6 +26,19 @@ class SchoolsController < ApplicationController
     else
       render action: 'new'
     end
+  end
+
+  def update
+    if @school.update_attributes(school_params)
+      redirect_to school_path(@school), notice: "Successfully updated #{@school.name}."
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @school.destroy
+    redirect_to schools_path, notice: "Successfully removed #{@school.name} from the system."
   end
 
   private
